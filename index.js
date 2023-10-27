cf9937c3668c12a69f96a87e78f04ecaaec3f60b
Vitor Bruno <vitorbrunorj@gmail.com>
2023-10-27 18:30:58 -0300

    base projeto.

---
+++ index.js
@@ -0,0 +1,19 @@
+function helloWord() {
+  console.log('Hello Word');
+}
+
+const saudacao = () => {
+  const data = new Date();
+  const hour = data.getHours();
+
+  if (hour <= 12) {
+    return 'Bom dia';
+  }
+  if (hour <= 18) {
+    return 'Boa tarde';
+  }
+  return 'boa noite';
+};
+
+helloWord();
+console.log(`A saudação é minha casa normal casa ${saudacao()}`);
